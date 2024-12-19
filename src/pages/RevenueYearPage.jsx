6cdb1f35403fa1b56f306c0d37c6abb2a0773dc1
Phat07 <nguyenhoangphat852@gmail.com>
2024-12-19 17:39:52 +0700

    chart logic code

--- src/pages/RevenueYearPage.jsx
+++ src/pages/RevenueYearPage.jsx
@@ -3,12 +3,44 @@ import { motion } from "framer-motion";
 import styles from "../css/reviewAppointment.module.css";
 import "../css/revenue.css";
 import { useEffect, useState } from "react";
+import { API } from "@/services/api";
+import { actGetSalonInformationByOwnerIdAsync } from "@/store/salonAppointments/action";
+import { useDispatch, useSelector } from "react-redux";
+import { Spin } from "antd";
 const RevenueYearPage = () => {
   const [startIndex, setStartIndex] = useState(0); // Vị trí bắt đầu hiển thị
   const [activeYear, setActiveYear] = useState(null); // Năm đang được chọn
   const years = Array.from({ length: 30 }, (_, i) => 2023 + i); // Danh sách năm từ 2020 đến 2030
   const visibleYears = years.slice(startIndex, startIndex + 4); // Lấy 4 năm để hiển thị
-  
+  const [isLoading, setIsLoading] = useState(false);
+  const [pieData, setPieData] = useState({
+    labels: [],
+    datasets: [],
+  });
+  const [lineData, setLineData] = useState({
+    labels: [],
+    datasets: [],
+  });
+  const [pieDataAppointment, setPieDataAppointment] = useState({
+    labels: [],
+    datasets: [],
+  });
+  const [lineDataAppointment, setLineDataAppointment] = useState({
+    labels: [],
+    datasets: [],
+  });
+  const dispatch = useDispatch();
+  const ownerId = useSelector((state) => state.ACCOUNT.idOwner);
+  const salonInformationByOwnerId = useSelector(
+    (state) => state.SALONAPPOINTMENTS.salonInformationByOwnerId
+  );
+
+  useEffect(() => {
+    if (ownerId) {
+      dispatch(actGetSalonInformationByOwnerIdAsync(ownerId));
+    }
+  }, [ownerId]);
+
   useEffect(() => {
     const currentYear = new Date().getFullYear();
     if (years.includes(currentYear)) {
@@ -16,10 +48,45 @@ const RevenueYearPage = () => {
     } else {
       setActiveYear(years[0]);
     }
-  }, []); 
+  }, []);
   useEffect(() => {
-    console.log("Active year updated:", activeYear);
-  }, [activeYear]);
+    const fetchData = async (url, params, setData) => {
+      setIsLoading(true);
+      try {
+        const response = await API.get(url, { params });
+        setData(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    const compileRevenueUrl = `/saloninformations/CompileRevenueSalonByYear/${salonInformationByOwnerId?.id}`;
+    const compileAppointmentUrl = `/saloninformations/CompileAppointmentSalonByYear/${salonInformationByOwnerId?.id}`;
+
+    // Fetch revenue data if salon ID and activeYear exist
+    if (salonInformationByOwnerId?.id && activeYear) {
+      fetchData(compileRevenueUrl, { year: activeYear }, (data) => {
+        setLineData(formatLineChartData(data));
+        setPieData(formatPieChartData(data));
+      });
+
+      // Fetch appointment data if salon ID and activeYear exist
+      fetchData(compileAppointmentUrl, { year: activeYear }, (data) => {
+        setLineDataAppointment(formatDataForLineChart(data.revenuewStatistics));
+        setPieDataAppointment(
+          formatDataForPieChart(
+            data.inSideAppointmentPercent,
+            data.outSideAppointmentPercent,
+            data.cancelAppointmentPercent,
+            data.failedAppointmentPercent
+          )
+        );
+      });
+    }
+  }, [activeYear, salonInformationByOwnerId]);
+
   const handleYearScroll = (direction) => {
     if (direction === "prev" && startIndex > 0) {
       setStartIndex(startIndex - 1);
@@ -31,49 +98,139 @@ const RevenueYearPage = () => {
     setActiveYear(year); // Cập nhật năm được chọn
     console.log("Selected year:", year); // Log để kiểm tra giá trị
   };
-  
-  const lineData = {
-    labels: [
-      "Tháng 1",
-      "Tháng 2",
-      "Tháng 3",
-      "Tháng 4",
-      "Tháng 5",
-      "Tháng 6",
-      "Tháng 7",
-      "Tháng 8",
-      "Tháng 9",
-      "Tháng 10",
-      "Tháng 11",
-      "Tháng 12",
-    ],
-    datasets: [
-      {
-        label: "Tổng lịch hẹn",
-        data: [10, 20, 15, 25, 30, 35, 40, 45, 50, 55, 60, 65],
-        borderColor: "#4F46E5",
-        backgroundColor: "rgba(79, 70, 229, 0.2)",
-      },
-      {
-        label: "Lịch hẹn ngoài hệ thống",
-        data: [5, 10, 7, 15, 20, 17, 22, 27, 32, 37, 42, 47],
-        borderColor: "#10B981",
-        backgroundColor: "rgba(16, 185, 129, 0.2)",
-      },
-    ],
+
+  const formatLineChartData = (data) => {
+    const labels = data.revenuewStatistics.map((item) => item.month);
+    const totalRevenueData = data.revenuewStatistics.map(
+      (item) => item.totalRevenue
+    );
+    const outSideRevenueData = data.revenuewStatistics.map(
+      (item) => item.outSideRevenue
+    );
+    const inSideRevenueData = data.revenuewStatistics.map(
+      (item) => item.inSideRevenue
+    );
+
+    return {
+      labels,
+      datasets: [
+        {
+          label: "Tổng doanh thu",
+          data: totalRevenueData,
+          borderColor: "#4F46E5",
+          backgroundColor: "rgba(79, 70, 229, 0.2)",
+        },
+        {
+          label: "Doanh thu ngoài hệ thống",
+          data: outSideRevenueData,
+          borderColor: "#10B981",
+          backgroundColor: "rgba(16, 185, 129, 0.2)",
+        },
+        {
+          label: "Doanh thu trong hệ thống",
+          data: inSideRevenueData,
+          borderColor: "#46e0f4",
+          backgroundColor: "rgba(0, 225, 255, 0.2)",
+        },
+      ],
+    };
   };
 
-  const pieData = {
-    labels: ["Tổng lịch hẹn", "Lịch hẹn ngoài hệ thống", "Lịch hẹn bị hủy"],
-    datasets: [
-      {
-        data: [65, 25, 10],
-        backgroundColor: ["#4F46E5", "#10B981", "#EF4444"],
-        hoverBackgroundColor: ["#4338CA", "#059669", "#DC2626"],
-      },
-    ],
+  const formatPieChartData = (data) => {
+    return {
+      labels: ["Doanh thu ngoài hệ thống", "Doanh thu trong hệ thống"],
+      datasets: [
+        {
+          data: [data.outSideRevenuePercent, data.inSideRevenuePercent],
+          backgroundColor: ["#10B981", "#46e0f4"],
+          hoverBackgroundColor: ["#059669", "#1c95a5"],
+        },
+      ],
+    };
   };
 
+  const formatDataForLineChart = (revenuewStatistics) => {
+    return {
+      labels: [
+        "Tháng 1",
+        "Tháng 2",
+        "Tháng 3",
+        "Tháng 4",
+        "Tháng 5",
+        "Tháng 6",
+        "Tháng 7",
+        "Tháng 8",
+        "Tháng 9",
+        "Tháng 10",
+        "Tháng 11",
+        "Tháng 12",
+      ],
+      datasets: [
+        {
+          label: "Lịch hẹn ngoài hệ thống",
+          data: revenuewStatistics.map((item) => item.outSideAppointment),
+          borderColor: "#10B981", // Xanh lá cây
+          backgroundColor: "rgba(16, 185, 129, 0.2)",
+        },
+        {
+          label: "Lịch hẹn trong hệ thống",
+          data: revenuewStatistics.map((item) => item.inSideAppointment),
+          borderColor: "#46e0f4", // Xanh dương nhạt
+          backgroundColor: "rgba(0, 225, 255, 0.2)",
+        },
+        {
+          label: "Lịch hẹn bị hủy",
+          data: revenuewStatistics.map((item) => item.cancelAppointment),
+          borderColor: "#F87171", // Đỏ
+          backgroundColor: "rgba(248, 113, 113, 0.2)",
+        },
+        {
+          label: "Lịch hẹn thất bại",
+          data: revenuewStatistics.map((item) => item.failedAppointment),
+          borderColor: "#FBBF24", // Vàng
+          backgroundColor: "rgba(251, 191, 36, 0.2)",
+        },
+      ],
+    };
+  };
+
+  const formatDataForPieChart = (
+    inSideAppointmentPercent,
+    outSideAppointmentPercent,
+    cancelAppointmentPercent,
+    failedAppointmentPercent
+  ) => {
+    return {
+      labels: [
+        "Lịch hẹn trong hệ thống",
+        "Lịch hẹn ngoài hệ thống",
+        "Lịch hẹn bị hủy",
+        "Lịch hẹn thất bại",
+      ],
+      datasets: [
+        {
+          data: [
+            inSideAppointmentPercent,
+            outSideAppointmentPercent,
+            cancelAppointmentPercent,
+            failedAppointmentPercent,
+          ],
+          backgroundColor: [
+            "#46e0f4", // Xanh dương nhạt
+            "#10B981", // Xanh lá cây
+            "#F87171", // Đỏ
+            "#FBBF24", // Vàng
+          ],
+          hoverBackgroundColor: [
+            "#1c95a5", // Hover xanh dương nhạt
+            "#059669", // Hover xanh lá cây
+            "#E11D48", // Hover đỏ
+            "#F59E0B", // Hover vàng
+          ],
+        },
+      ],
+    };
+  };
   return (
     <div
       className={`${styles.appointmentContainer} bg-customGray min-h-screen p-4 sm:p-8`}
@@ -132,21 +289,26 @@ const RevenueYearPage = () => {
       {/* Biểu đồ thống kê lịch hẹn */}
       <div className="bg-white p-4 sm:p-6 rounded-lg shadow-md mb-4 sm:mb-6">
         <h2 className="text-lg sm:text-xl font-bold mb-4">THỐNG KÊ LỊCH HẸN</h2>
-        <div className="flex flex-col sm:flex-row gap-4">
-          {/* Biểu đồ đường lớn hơn */}
-          <div className="w-full sm:w-2/3">
-            <div className="h-64 sm:h-80 lg:h-96">
-              <Line data={lineData} options={{ maintainAspectRatio: false }} />
+        <Spin className="custom-spin" spinning={isLoading}>
+          <div className="flex flex-col sm:flex-row gap-4">
+            <div className="w-full sm:w-2/3">
+              <div className="h-64 sm:h-80 lg:h-96">
+                <Line
+                  data={lineDataAppointment}
+                  options={{ maintainAspectRatio: false }}
+                />
+              </div>
+            </div>
+            <div className="w-full sm:w-1/3">
+              <div className="h-64 sm:h-80">
+                <Pie
+                  data={pieDataAppointment}
+                  options={{ maintainAspectRatio: false }}
+                />
+              </div>
             </div>
           </div>
-
-          {/* Biểu đồ tròn */}
-          <div className="w-full sm:w-1/3">
-            <div className="h-64 sm:h-80">
-              <Pie data={pieData} options={{ maintainAspectRatio: false }} />
-            </div>
-          </div>
-        </div>
+        </Spin>
       </div>
 
       {/* Biểu đồ thống kê doanh thu */}
@@ -154,21 +316,25 @@ const RevenueYearPage = () => {
         <h2 className="text-lg sm:text-xl font-bold mb-4">
           THỐNG KÊ DOANH THU
         </h2>
-        <div className="flex flex-col sm:flex-row gap-4">
-          {/* Biểu đồ đường lớn hơn */}
-          <div className="w-full sm:w-2/3">
-            <div className="h-64 sm:h-80 lg:h-96">
-              <Line data={lineData} options={{ maintainAspectRatio: false }} />
+        <Spin className="custom-spin" spinning={isLoading}>
+          <div className="flex flex-col sm:flex-row gap-4">
+            {/* Biểu đồ đường lớn hơn */}
+            <div className="w-full sm:w-2/3">
+              <div className="h-64 sm:h-80 lg:h-96">
+                <Line
+                  data={lineData}
+                  options={{ maintainAspectRatio: false }}
+                />
+              </div>
+            </div>
+            {/* Biểu đồ tròn */}
+            <div className="w-full sm:w-1/3">
+              <div className="h-64 sm:h-80">
+                <Pie data={pieData} options={{ maintainAspectRatio: false }} />
+              </div>
             </div>
           </div>
-
-          {/* Biểu đồ tròn */}
-          <div className="w-full sm:w-1/3">
-            <div className="h-64 sm:h-80">
-              <Pie data={pieData} options={{ maintainAspectRatio: false }} />
-            </div>
-          </div>
-        </div>
+        </Spin>
       </div>
     </div>
   );
